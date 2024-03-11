7fa5f6c63f67eae8daea1813cf6e7a3da0553241
nicolazin <106328512+nicolazin@users.noreply.github.com>
2024-03-11 12:13:36 -0300

    meio do  dia 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
-for (let i =0; 1 <botoes.length; i++) {
-    botoes[i].onclick = fimction () {
+for (let i =0; i <botoes.length; i++) {
+    botoes[i].onclick = function () {
         for (let j=0; j<botoes.length; j++){
             botoes [j].classList.remove("ativo");
         }
