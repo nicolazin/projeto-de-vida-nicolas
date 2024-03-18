5175bafc66a3508006c938a94319ed0fa0543ecc
nicolazin <106328512+nicolazin@users.noreply.github.com>
2024-03-18 07:46:38 -0300

    de hojje

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
         for (let j= 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
-            texto[i].classList.remove("ativo");
+            texto[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
         texto[i].classList.add("ativo");
